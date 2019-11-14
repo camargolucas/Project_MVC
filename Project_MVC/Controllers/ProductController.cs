063bb46d425d6ddfccaaa3bce51ef2c51ec2dc69
Lucas <lucas.camargo.silva98@gmail.com>
2019-11-14 11:41:02 -0300

    first and last commit

---
+++ Project_MVC/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_MVC.ProductService;
+
+namespace Project_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        readonly ProductServiceClient _wcf = new ProductServiceClient();
+        public ActionResult Index()
+        {
+            List<TB_PRODUCT> products = _wcf.getProducts().ToList();
+            return View(products);
+        }
+    }
+}
